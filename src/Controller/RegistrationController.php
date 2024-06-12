eaf88c73aa481a8b2191306d2c0191c5cc3757ae
Your Name <amauryjouan@icloud.com>
2024-06-12 12:18:19 +0200

    ✨

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,11 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
+    
+        // Vérifie si l'utilisateur est déjà connecté
+        if ($this->getUser()) {
+        return $this->redirectToRoute('app_home');
+        }
         $user = new Demandeur();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);

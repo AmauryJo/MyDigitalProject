b662ea886843e5577d778861f6628dea1993bd58
Your Name <amauryjouan@icloud.com>
2024-06-12 17:17:50 +0200

    ✨ Connexion fonctionnel

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Psr\Log\LoggerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, LoggerInterface $logger): Response
     {
         // Vérifie si l'utilisateur est déjà connecté
         if ($this->getUser()) {
@@ -21,6 +22,12 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // Récupère le dernier nom d'utilisateur entré par l'utilisateur
         $lastUsername = $authenticationUtils->getLastUsername();
+        
+        // Log des informations pour le débogage
+        $logger->info('Tentative de connexion', [
+            'last_username' => $lastUsername,
+            'error' => $error ? $error->getMessage() : 'Aucune erreur'
+        ]);
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,

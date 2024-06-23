0cf136aa58614c22c516f4cb4f7eb71abd235895
Your Name <amauryjouan@icloud.com>
2024-06-23 22:08:14 +0200

    Finalisation du site avec plugin accessibilité

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\DemandeurRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,12 +11,6 @@ class BlogController extends AbstractController
     #[Route('/blog', name: 'app_blog')]
     public function blog(): Response
     {
-        // $demandeurs = $demandeurRepository->findAll();
-        // $demandeur = $demandeurRepository->findOneById(1);
-        // return $this->render('home/home.html.twig',[
-        //     'demandeur' => $demandeur,
-        // ]);
-
         return $this->render('blog/blog.html.twig', [
         ]);
     }
